e38b743819ca21a5a4a6fc9ee1bb92a4f1432915
kyryllkotyk <kotykkyryll@gmail.com>
2025-07-06 10:25:08 -0700

    Add task display (regular, diff, est time). Add a method for creating and adding the task, for use by main. Add sorting, and comments

--- JustToastIt/TaskCollection.cpp
+++ JustToastIt/TaskCollection.cpp
@@ -6,19 +6,52 @@ TaskCollection::TaskCollection()
 	currentIDAssignment = 0;
 }
 
+void TaskCollection::displayTasks()
+{
+	//TODO:: Add mutex
+	displayHelper(tasks);
+}
+
+void TaskCollection::displayTasksByDifficulty()
+{
+	std::vector<Task> tasksCopy = tasks;
+	quickSort(tasksCopy, tasksCopy.size() - 1, 0, 'd');
+	displayHelper(tasksCopy);
+}
+
+void TaskCollection::displayTasksByEstimatedTime()
+{
+	std::vector<Task> tasksCopy = tasks;
+	quickSort(tasksCopy, tasksCopy.size() - 1, 0, 't');
+	displayHelper(tasksCopy);
+}
+
+
 void TaskCollection::addTask(Task& task)
 {
 	task.setID(currentIDAssignment);
 	
 	tasks.push_back(task);
-	LOG("TaskCollection", "Added Task with ID " + currentIDAssignment, false);
+	LOG("TaskCollection", "Added Task with ID " + 
+		std::to_string(currentIDAssignment), false);
 	currentIDAssignment++;
+}
+
+void TaskCollection::createAndAddTask(std::string& name, short estDiff,
+	int estTime, std::string& dueDate)
+{
+	// Create task from given parameters
+	Task task(name, -1, estDiff, estTime, dueDate);
+	// Add this task to the collection
+	(*this).addTask(task);
 }
 
 bool TaskCollection::removeTask(const std::string& taskName)
 {
 	for (int i = 0; i < tasks.size(); i++) {
+		// Checks whether the element's name matches the name to remove
 		if (tasks[i].getName() == taskName) {
+			// Erases the task at the found position
 			tasks.erase(tasks.begin() + i);
 			LOG("TaskCollection", "RemoveTaskByName: Removed " + taskName, false);
 			return true;
@@ -31,7 +64,9 @@ bool TaskCollection::removeTask(const std::string& taskName)
 bool TaskCollection::removeTask(const int id)
 {
 	for (int i = 0; i < tasks.size(); i++) {
+		// Checks whether the element's ID matches the ID to remove
 		if (tasks[i].getID() == id) {
+			// Erases the task at the found position
 			tasks.erase(tasks.begin() + i);
 			LOG("TaskCollection", "RemoveTaskByID: Removed " + std::to_string(id), false);
 			return true;
@@ -39,4 +74,63 @@ bool TaskCollection::removeTask(const int id)
 	}
 	LOG("TaskCollection", "RemoveTaskByID: ID not found in collection", true);
 	return false;
+}
+
+
+int TaskCollection::partition(std::vector<Task>& vec, int high, 
+	int low, char sortBy) {
+	
+	int pivot = -1;
+	switch (sortBy) {
+	case 't':
+		pivot = vec[high].getEstimatedTime();
+		break;
+	case 'd':
+		pivot = vec[high].getDifficulty();
+		break;
+	}
+
+	int lowerIndex = low - 1;
+
+	// Move all smaller elements on the left, after every iteration,
+	// the values between low and lowerIndex are smaller
+	for (int i = low; i <= high - 1; i++) {
+		switch (sortBy) {
+		case 't':
+			if (vec[i].getEstimatedTime() < pivot) {
+				lowerIndex++;
+				std::swap(vec[i], vec[lowerIndex]);
+			}
+			break;
+		case 'd':
+			if (vec[i].getDifficulty() < pivot) {
+				lowerIndex++;
+				std::swap(vec[i], vec[lowerIndex]);
+			}
+			break;
+		}
+		
+	}
+
+	std::swap(vec[lowerIndex + 1], vec[high]);
+	return lowerIndex + 1;
+}
+
+void TaskCollection::displayHelper(std::vector<Task>& vec)
+{
+	for (int i = 0; i < vec.size(); i++) {
+		std::cout << "[Name] " << vec[i].getName() << " "
+			<< "[Estimated Time (min)] " << vec[i].getEstimatedTime() << " "
+			<< "[Difficulty] " << vec[i].getDifficulty() << " "
+			<< "[Due Date (min)] " << vec[i].getDueDateInMinutes() << "\n";
+	}
+}
+
+void TaskCollection::quickSort(std::vector<Task>& vec, int high, int low, char sortBy)
+{
+	if (low < high) {
+		int partitionIndex = partition(vec, high, low, sortBy);
+		quickSort(vec, partitionIndex - 1, low, sortBy);
+		quickSort(vec, high, partitionIndex + 1, sortBy);
+	}
 }
--- JustToastIt/TaskCollection.h
+++ JustToastIt/TaskCollection.h
@@ -8,17 +8,34 @@ class TaskCollection
 {
 public:
 	TaskCollection();
-	void displayTasksByPriority();
+
+	void displayTasksByPriority(); // Will not be added in MVP
 	void displayTasks();
-	void displayTasksByDueDate();
-	void displayTasksByDifficulty();
-	void displayTasksByCompletionTime();
+	void displayTasksByDueDate(); // Will not be added in MVP
+	void displayTasksByDifficulty(); // In ascending order
+	void displayTasksByEstimatedTime(); // In ascending order
+	
+	//TODO:: Either ensure unique names OR remove ability to delete by name
+	// likely second option, as the system will always know the ID of the task
+	// the user marked for deletion
+	
+	//TODO:: Ensure correct handling for misinput
+	
 	//add task should set the unique id to the task
 	void addTask(Task& task); // called from main
+
+	void createAndAddTask(std::string& name, short estDiff,
+		int estTime, std::string& dueDate);
+
+
 	bool removeTask(const std::string& taskName);
+	
 	bool removeTask(const int id);
 
 private:
+	void quickSort(std::vector<Task>& vec, int high, int low, char sortBy);
+	int partition(std::vector<Task>& vec, int high, int low, char sortBy);
+	void displayHelper(std::vector<Task>& vec);
 	int currentIDAssignment;
 	std::vector<Task> tasks;
 };
