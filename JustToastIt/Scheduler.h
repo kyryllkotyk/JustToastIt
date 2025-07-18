65e96136d94d1dc0f2e12120added631c3caf8e3
kyryllkotyk <kotykkyryll@gmail.com>
2025-07-18 15:17:32 -0700

    Update scheduler logic

--- JustToastIt/Scheduler.h
+++ JustToastIt/Scheduler.h
@@ -1,16 +1,30 @@
 #ifndef SCHEDULER_H_
 #define SCHEDULER_H_
 
-#include "Task.h"
-#include <map>
-//DO NOT TOUCH FURTHER UNTIL TASK COLLECTION IS TESTED
+#include "TaskCollection.h"
+#include "Toast.h"
+
 class Scheduler
 {
 public:
-	void scheduleTask(const Task& task);
+	Scheduler() = default;
 private:
-	std::multimap<TIME_POINT, Task> scheduledTasks;
-	void calculateTask(const Task& task, TIME_POINT& point);
+	void threadWorking(const TaskCollection& collection);
+	std::mutex schedulerMutex;
+	std::thread processingThread; // Background thread for pushing toasts
+	Toast toaster;
+
+	Scheduler(Scheduler& schedule) = delete;
+	// could make task collection check all task's dueDateInMinutes
+	// and if it reaches zero, call the scheduler
+	// OR
+	// the scheduler itself is responsible for checking the due dates
+	// so it calls the task collection 
+	// opt1 seems better, TODO:: investigate
+
+	//in a thread, wake up once a minute, check whether the time point due date 
+	//matches now() !!MINUTE PRECISION!! for every Task, if any match, add them to
+	//Toast's queue
 
 };
 
